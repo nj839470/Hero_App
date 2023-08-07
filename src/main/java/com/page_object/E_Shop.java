1d3d89f5ba7a19926ec8fdf8f5ce34715c7e7882
nj839470 <nj839470@gmail.com>
2023-08-08 04:57:42 +0530

    .

--- src/main/java/com/page_object/E_Shop.java
+++ src/main/java/com/page_object/E_Shop.java
@@ -12,21 +12,31 @@ public class E_Shop extends Base_Utility {
 		PageFactory.initElements(driver, this);
 	}
 
-	@FindBy(xpath = "//android.widget.FrameLayout[@content-desc=\"Eshop\"]/android.view.ViewGroup/android.widget.TextView")
+	@FindBy(xpath = "//android.widget.FrameLayout[@content-desc='Eshop']/android.view.ViewGroup/android.widget.TextView")
 	private WebElement E_shop;
-	@FindBy(xpath = "//android.view.View[@content-desc=\"cart\"]")
+	@FindBy(xpath = "//android.view.View[@content-desc='cart']")
 	private WebElement shoping_cart;
-	@FindBy(xpath = "//android.view.View[@content-desc=\"wishlist\"]")
+	@FindBy(xpath="//android.view.View[contains(@text,'You have no items')]")
+	private WebElement shoping_cart_items;
+	@FindBy(xpath = "//android.view.View[@content-desc='wishlist']")
 	private WebElement wishlist;
 	@FindBy(xpath = "//android.view.View[@resource-id ='block-customer-login-heading']")
 	private WebElement customer_login_heading;
+	@FindBy(xpath="//android.view.View[contains(@text,'If you have an account')]")
+	private WebElement registered_custombers_message;
 	@FindBy(xpath = "//android.widget.EditText[@resource-id ='email']")
 	private WebElement enter_email_or_mobile_number;
-	@FindBy(xpath ="//android.view.View[@content-desc=\"Continue\"]")
+	@FindBy(xpath ="//android.view.View[@content-desc='Continue']")
 	private WebElement Continue_button;
+	@FindBy(xpath ="//android.view.View[contains(@text, 'Verify your mobile')]")
+	private WebElement verify_mobile;
+	@FindBy(xpath ="//android.view.View[contains(@text,'Please enter 4-Digit OTP')]")
+	private WebElement verify_mobile_message;
+	@FindBy(xpath ="//android.view.View[@resource-id ='otp-pop-up-close']")
+	private WebElement otp_pop_up_close;
 	@FindBy(xpath ="//android.view.View[@resource-id ='block-new-customer-heading']")
 	private WebElement new_customer_heading;
-	@FindBy(xpath ="//android.view.View[@content-desc=\"CREATE AN ACCOUNT\"]/android.widget.TextView")
+	@FindBy(xpath ="//android.view.View[@content-desc= 'CREATE AN ACCOUNT']/android.widget.TextView")
 	private WebElement Create_Account;
 	public WebElement E_shop()
 	{
@@ -36,6 +46,10 @@ public class E_Shop extends Base_Utility {
 	{
 		return shoping_cart;
 	}
+	public WebElement shoping_cart_items()
+	{
+		return shoping_cart_items;
+	}
 	public WebElement wishlist()
 	{
 		return wishlist;
@@ -43,6 +57,10 @@ public class E_Shop extends Base_Utility {
 	public WebElement customer_login_heading()
 	{
 		return customer_login_heading;
+	}
+	public WebElement registered_custombers_message()
+	{
+		return registered_custombers_message;
 	}
 	public WebElement enter_email_or_mobile_number()
 	{
@@ -52,6 +70,18 @@ public class E_Shop extends Base_Utility {
 	{
 		return Continue_button;
 	}
+	public WebElement verify_mobile()
+	{
+		return verify_mobile;
+	}
+	public WebElement verify_mobile_message()
+	{
+		return verify_mobile_message;
+	}
+	public WebElement otp_pop_up_close()
+	{
+		return otp_pop_up_close;
+	}
 	public WebElement new_customer_heading()
 	{
 		return new_customer_heading;
@@ -59,5 +89,102 @@ public class E_Shop extends Base_Utility {
 	public WebElement Create_Account()
 	{
 		return Create_Account;
+	}
+	//*********************************** new_customers ******************************
+	@FindBy(xpath ="//android.view.View[@content-desc='javascript:void(0);']")
+	private WebElement user_account;
+	@FindBy(xpath ="//android.view.View[@resource-id = 'block-new-customer-heading']")
+	private WebElement newcustomer_heading;
+	@FindBy(xpath ="//android.view.View[contains(@text,'Creating an account')]")
+	private WebElement newcustomer_message1;
+	@FindBy(xpath="//android.widget.TextView[contains(@text ,'See order and')]")
+	private WebElement newcustomer_message2;
+	@FindBy(xpath ="//android.widget.TextView[contains(@text ,'Track order')]")
+	private WebElement newcustomer_message3;
+	@FindBy(xpath ="//android.widget.TextView[contains(@text ,'Check out faster')]")
+	private WebElement newcustomer_message4;
+	@FindBy(xpath ="//android.view.View[@content-desc='Create an Account']/android.widget.TextView")
+	private WebElement create_account;
+	@FindBy(xpath ="//android.widget.EditText[@resource-id = 'firstname']")
+	private WebElement firstname;
+	@FindBy(xpath ="//android.widget.EditText[@resource-id = 'lastname']")
+	private WebElement lastname;
+	@FindBy(xpath ="//android.widget.CheckBox[@resource-id ='is_subscribed']")
+	private WebElement CheckBox;
+	@FindBy(xpath ="//android.widget.EditText[@resource-id ='mobile']")
+	private WebElement mobile;
+	@FindBy(xpath ="//android.view.View[@content-desc='Get OTP']/android.widget.TextView")
+	private WebElement OTP;
+	@FindBy(xpath ="//android.widget.EditText[@resource-id ='email_address']")
+	private WebElement email_address;
+	@FindBy(xpath ="//android.widget.CheckBox[@resource-id ='custom-checkbox']")
+	private WebElement checkbox_for_T_and_C;
+	@FindBy(xpath="//android.widget.Button[@resource-id ='register-submit']")
+	private WebElement create_an_account;
+	@FindBy(xpath ="//android.widget.TextView[@text ='BACK']")
+	private WebElement back_button;
+	public WebElement user_account()
+	{
+		return user_account;
+	}
+	public WebElement newcustomer_heading()
+	{
+		return newcustomer_heading;
+	}
+	public WebElement newcustomer_message1()
+	{
+		return newcustomer_message1;
+	}
+	public WebElement newcustomer_message2()
+	{
+		return newcustomer_message2;
+	}
+	public WebElement newcustomer_message3()
+	{
+		return newcustomer_message3;
+	}
+	public WebElement newcustomer_message4()
+	{
+		return newcustomer_message4;
+	}
+	public WebElement create_account()
+	{
+		return create_account;
+	}
+	public WebElement firstname()
+	{
+		return firstname;
+	}
+	public WebElement lastname()
+	{
+		return lastname;
+	}
+	public WebElement CheckBox()
+	{
+		return CheckBox;
+	}
+	public WebElement mobile()
+	{
+		return mobile;
+	}
+	public WebElement email_address()
+	{
+		return email_address;
+	}
+	public WebElement OTP()
+	{
+		return OTP;
+	}
+	public WebElement checkbox_for_T_and_C()
+	{
+		return checkbox_for_T_and_C;
+	}
+	public WebElement create_an_account()
+	{
+		return create_an_account;
+	}
+	public WebElement back_button()
+	{
+		return back_button;
 	}
 }
--- src/test/java/Page_Test/E_Shop_test.java
+++ src/test/java/Page_Test/E_Shop_test.java
@@ -1,18 +1,86 @@
 package Page_Test;
 
+import org.testng.annotations.Test;
+
 import com.page_object.E_Shop;
 import com.utility.Base_Utility;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class E_Shop_test extends Base_Utility{
 	public Login_Page_Test login;
 	public E_Shop ob;
-	public void registered_customers()
+	@Test(priority = 0)
+	public void registered_customers() throws InterruptedException
 	{
 		msg("************************E Shop**************************");
 		ob = new E_Shop();
-//		login = new Login_Page_Test();
-//		login.login();
+		login = new Login_Page_Test();
+		login.login();
+		Thread.sleep(5000);
+		try {
 		Custom_click(ob.E_shop(), ob.E_shop().getText());
+		Thread.sleep(5000);
+		try {
+		Custom_click(ob.shoping_cart(), "Shoping Cart list");
+		Thread.sleep(2000);
+		}catch(Exception e) { System.err.println(e);}
+		try {
+		msg(ob.shoping_cart_items().getText());
+		}catch(Exception e) { System.err.println(e);}
+		try {
+		Custom_click(ob.wishlist(), "wishlist");
+		Thread.sleep(2000);
+		}catch(Exception e) { System.err.println(e);}
+		try {
+		msg("for " +ob.customer_login_heading().getText());
+		msg(ob.registered_custombers_message().getText());
+		}catch(Exception e) { System.err.println(e);}
+		custom_sendkeys(ob.enter_email_or_mobile_number(), "rahul@gmail.com", "Email ID");
+		driver.navigate().back();
+		Custom_click(ob.Continue_button(), "Continue button");
+		msg(ob.verify_mobile().getText());
+		msg(ob.verify_mobile_message().getText());
+		Custom_click(ob.otp_pop_up_close(), "OTP Pop Up close");
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 	}
-
+	@Test(priority = 1)
+	public void new_customers() {
+		try {
+		Custom_click(ob.user_account(), "User account");
+		msg(ob.newcustomer_heading().getText());
+		msg(ob.newcustomer_message1().getText());
+		msg(ob.newcustomer_message2().getText());
+		msg(ob.newcustomer_message3().getText());
+		msg(ob.newcustomer_message4().getText());
+		Custom_click(ob.create_account(), ob.create_account().getText());
+		custom_sendkeys(ob.firstname(), "Sonu", " First name ");
+		((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.TAB));
+		((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.TAB));
+		custom_sendkeys(ob.lastname(), "Jindal", " Last name ");
+		((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.TAB));
+		Custom_click(ob.CheckBox(), ob.CheckBox().getText());
+		custom_sendkeys(ob.mobile(), "9876564534", " Mobile number ");
+		((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.TAB));
+		custom_sendkeys(ob.email_address(), "sonu@gmail.com", " Email ");
+		((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.TAB));
+		Custom_click(ob.checkbox_for_T_and_C(), " Tearms and Condition check Box ");
+		Custom_click(ob.OTP(), ob.OTP().getText());
+		msg(ob.verify_mobile().getText());
+		msg(ob.verify_mobile_message().getText());
+		Custom_click(ob.otp_pop_up_close(), "OTP Pop Up close");
+		Custom_click(ob.create_an_account(), ob.create_an_account().getText());
+		Thread.sleep(2000);
+		driver.navigate().back();
+		Custom_click(ob.back_button(), "Back from Create New Customer Account");
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
 }
